d9be457e03bacb024cacd662cd3129740044432b
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-30 15:19:34 -0300

    feat: adiciona data e hora nos metodos de tarefa

--- tarefa.py
+++ tarefa.py
@@ -5,6 +5,7 @@ class Tarefa:
         self.titulo = titulo
         self.descricao = descricao
         self.concluida = False
+        self.dataConclusao = None
 
     def estaConcluida(self):
         if self.concluida:
@@ -14,23 +15,28 @@ class Tarefa:
 
     def marcarComoConcluido(self):
         self.concluida = True
+        self.dataConclusao = datetime.now().strftime("%d/%m/%Y às %H:%M")
 
     def marcarComoPendente(self):
         self.concluida = False
+        self.dataConclusao = None
 
     def __str__(self):
         status = "+" if self.concluida else " "
-        return f"[{status}] {self.titulo}: {self.descricao}"
+        infoData = f"(Concluída em: {self.dataConclusao})" if self.concluida else ""
+        return f"[{status}] {self.titulo}: {self.descricao}{infoData}"
 
     def paraDict(self):
         return {
             "titulo": self.titulo,
             "descricao": self.descricao,
-            "concluida": self.concluida
+            "concluida": self.concluida,
+            "dataConclusao": self.dataConclusao
         }
 
     @classmethod
     def deDict(cls, dados):
         novaTarefa = cls(dados["titulo"], dados["descricao"])
         novaTarefa.concluida = dados["concluida"]
+        novaTarefa.dataConclusao = dados.get("dataConclusao", None)
         return novaTarefa
