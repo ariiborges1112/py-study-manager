73f9936749960b0a498ec2d81e6d8d551fb94e41
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-30 17:40:14 -0300

    chore: remove verificacoes fantasmas nos metodos de listagem

--- gerenciadorTarefas.py
+++ gerenciadorTarefas.py
@@ -10,52 +10,25 @@ class GerenciadorTarefas:
         print(f"\nA Tarefa '{tarefa.titulo}' adicionada com sucesso!")
 
     def listarTarefas(self):
-        if not self.tarefas:
-            print("\nA lista de tarefas está vazia\n")
-            return
-    
         print("\n==== LISTA DE TAREFAS ====")
         for i, tarefa in enumerate(self.tarefas, start=1):
             print(f"Tarefa {i}:\n{tarefa}\n")
 
     def listarTarefasConcluidas(self):
-        if not self.tarefas:
-            print("\nA lista de tarefas está vazia\n")
-            return
-
-        temConcluidas = False
-
         print("\n==== TAREFAS CONCLUÍDAS ====")
         for i, tarefa in enumerate(self.tarefas, start=1):
             if tarefa.concluida:
-                temConcluidas = True
                 print(f"ID da Tarefa: [{i}]")
                 print(f"{tarefa.titulo}")
                 print(f"{tarefa.descricao}\n")
 
-        if not temConcluidas:
-            print("Não há tarefas concluídas!")
-            return
-
     def listarTarefasNaoConcluidas(self):
-        if not self.tarefas:
-            print("\nA lista de tarefas está vazia\n")
-            return
-
-        temPendencias = False
-
-
         print("\n==== TAREFAS NÃO CONCLUÍDAS ====")
         for i, tarefa in enumerate(self.tarefas, start=1):
             if not tarefa.concluida:
-                temPendencias = True
                 print(f"ID da Tarefa: [{i}]")
                 print(f"{tarefa.titulo}")
                 print(f"{tarefa.descricao}\n")
-
-        if not temPendencias:
-            print("Não há tarefas pendentes, parabéns!")
-            return
 
     def concluirTarefaViaIndice(self, indice):
         if indice < 0 or indice >= len(self.tarefas):
